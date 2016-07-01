2cf1c25a035faf05836569a22a47b26ed52061c8
Patxi Llano <patxillano@gmail.com>
2016-07-01 10:34:36 +0200

    Formulario mejor adaptado

--- asignaturas/forms.py
+++ asignaturas/forms.py
@@ -2,22 +2,18 @@ from django import forms
 from django.contrib.auth.models import User
 from django.forms.widgets import SelectDateWidget
 from datetime import datetime
-from .models import calendar
 
 
 
-class courseForm(forms.ModelForm):
-    class Meta:
-		model = calendar
-		fields = ('cursoAcademico', 'inicioPrimerCuatrimestreDia', 'inicioPrimerCuatrimestreMes', 'finPrimerCuatrimestreDia', 'finPrimerCuatrimestreMes', 'inicioSegundoCuatrimestreDia', 'inicioSegundoCuatrimestreMes', 'finSegundoCuatrimestreDia', 'finSegundoCuatrimestreMes')
-    def __init__(self, *args, **kwargs):
-        super(courseForm, self).__init__(*args, **kwargs)
-        self.fields['cursoAcademico'].widget.attrs.update({'class': 'form-input'})
-        self.fields['inicioPrimerCuatrimestreDia'].widget.attrs.update({'class': 'form-input'})
-        self.fields['inicioPrimerCuatrimestreMes'].widget.attrs.update({'class': 'form-input'})
-        self.fields['finPrimerCuatrimestreDia'].widget.attrs.update({'class': 'form-input'})
-        self.fields['finPrimerCuatrimestreMes'].widget.attrs.update({'class': 'form-input'})
-        self.fields['inicioSegundoCuatrimestreDia'].widget.attrs.update({'class': 'form-input'})
-        self.fields['inicioSegundoCuatrimestreMes'].widget.attrs.update({'class': 'form-input'})
-        self.fields['finSegundoCuatrimestreDia'].widget.attrs.update({'class': 'form-input'})
-        self.fields['finSegundoCuatrimestreMes'].widget.attrs.update({'class': 'form-input'})
+CURSOS = (
+    ('16/17', '2016/2017'),
+    ('17/18', '2017/2018')
+)
+
+
+class courseForm(forms.Form):
+    cursoAcademico = forms.ChoiceField(choices=CURSOS)
+    inicioCuatrimestreUno = forms.DateField(widget=forms.SelectDateWidget())
+    finCuatrimestreUno = forms.DateField(widget=forms.SelectDateWidget())
+    inicioCuatrimestreDos = forms.DateField(widget=forms.SelectDateWidget())
+    finCuatrimestreDos = forms.DateField(widget=forms.SelectDateWidget())
--- asignaturas/models.py
+++ asignaturas/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from django import forms
 from datetime import date, datetime
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
@@ -145,68 +146,3 @@ class Matricula(models.Model):
     usuario = models.ForeignKey(User)
     grupo = models.ForeignKey(Grupo)
     grado = models.ForeignKey(Grado)
-
-
-class calendar(models.Model):
-    DIAS = (
-        ('1', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
-        ('5', '5'),
-        ('6', '6'),
-        ('7', '7'),
-        ('8', '8'),
-        ('9', '9'),
-        ('10', '10'),
-        ('11', '11'),
-        ('12', '12'),
-        ('13', '13'),
-        ('14', '14'),
-        ('15', '15'),
-        ('16', '16'),
-        ('17', '17'),
-        ('18', '18'),
-        ('19', '19'),
-        ('20', '20'),
-        ('21', '21'),
-        ('22', '22'),
-        ('23', '23'),
-        ('24', '24'),
-        ('25', '25'),
-        ('26', '26'),
-        ('27', '27'),
-        ('28', '28'),
-        ('29', '29'),
-        ('30', '30'),
-        ('31', '31')
-    )
-    MESES = (
-        ('1', 'Enero'),
-        ('2', 'Febrero'),
-        ('3', 'Marzo'),
-        ('4', 'Abril'),
-        ('5', 'Mayo'),
-        ('6', 'Junio'),
-        ('7', 'Julio'),
-        ('8', 'Agosto'),
-        ('9', 'Septiembre'),
-        ('10', 'Octubre'),
-        ('11', 'Noviembre'),
-        ('12', 'Diciembre')
-    )
-
-    CURSOS = (
-        ('2016/2017', '16/17'),
-        ('2017/2018', '17/18')
-    )
-
-    cursoAcademico = models.IntegerField(choices=CURSOS, blank=True)
-    inicioPrimerCuatrimestreDia = models.IntegerField(choices=DIAS, blank=True)
-    inicioPrimerCuatrimestreMes = models.IntegerField(choices=MESES, blank=True)
-    finPrimerCuatrimestreDia = models.IntegerField(choices=DIAS, blank=True)
-    finPrimerCuatrimestreMes = models.IntegerField(choices=MESES, blank=True)
-    inicioSegundoCuatrimestreDia = models.IntegerField(choices=DIAS, blank=True)
-    inicioSegundoCuatrimestreMes = models.IntegerField(choices=MESES, blank=True)
-    finSegundoCuatrimestreDia = models.IntegerField(choices=DIAS, blank=True)
-    finSegundoCuatrimestreMes = models.IntegerField(choices=MESES, blank=True)
